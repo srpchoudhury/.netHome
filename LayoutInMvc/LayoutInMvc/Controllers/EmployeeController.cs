4a1b73b24dfa36c6c6aba1ade93d15a6a4225ba7
S Rudra Prasad Choudhury <srudra2021@gift.edu.in>
2023-09-24 15:28:48 +0530

    commit

---
+++ LayoutInMvc/LayoutInMvc/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LayoutInMvc.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        [Route("Employees")]
+        public ActionResult GetList()
+        {
+            return View();
+        }
+
+        [Route("NewEmployees")]
+        public ActionResult AddEmployee()
+        {
+            return View();
+        }
+    }
+}
---
+++ LayoutInMvc/LayoutInMvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LayoutInMvc.MvcApplication" Language="C#" %>
