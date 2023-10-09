b7d256758b5ee18abc7933d29619e73e86e6582c
S Rudra Prasad Choudhury <srudra2021@gift.edu.in>
2023-10-09 07:52:41 +0530

    commit

---
+++ axis/axis/Controllers/AxisBankHomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace axis.Controllers
+{
+    public class AxisBankHomeController : Controller
+    {
+        // GET: AxisBankHome
+        public ActionResult Landing()
+        {
+            return View();
+        }
+
+
+        //Login Form Action
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
